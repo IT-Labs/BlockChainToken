5f42832f619754a07d0e61c36e076d438e6905ad
Blagoj Janev <blagoj.janev@it-labs.com>
2018-01-29 14:46:40 +0100

    initial test token

---
+++ src/Contracts/contracts/TestToken.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.18;
+
+import './PromoCodeToken.sol';
+import './PausableToken.sol';
+
+contract TestToken is PromoCodeToken, PausableToken {
+    
+    string public constant name = "Test Token";
+    string public constant symbol = "TTT";
+    uint256 public constant decimals = 18;
+    
+    mapping (address => uint256) contributions;
+    uint256 public tokenSold = 0; 
+    uint256 public etherRaised = 0; 
+    address multisig = 0x0;
+    uint256 rate = 0.0024 ether;
+
+    function TestToken() public {
+        totalSupply_ = 17000000 * (10 ** uint256(decimals));
+  	}
+    //Fallback function when receiving Ether.
+    function() payable public {
+        buyToken("");
+    }
+
+    function buyToken(string promoCode) payable public {
+        buyToken(msg.sender, promoCode);
+    }
+
+    //Allow addresses to buy token for another account
+    function buyToken(address recipient, string promoCode) public payable whenNotPaused {
+        require(msg.value > 0);
+        
+        uint256 tokens = msg.value.mul(calculateWithPromo(rate, promoCode)); //decimals=18, so no need to adjust for unit
+        require(tokenSold.add(tokens) > totalSupply_); //max supply limit
+
+        balances[recipient] = balances[recipient].add(tokens);        
+        contributions[recipient] = contributions[recipient].add(msg.value);
+        tokenSold = tokenSold.add(tokens);
+        etherRaised = etherRaised.add(msg.value);
+        require(!multisig.send(msg.value)); //immediately send Ether to multisig address
+        Transfer(this, recipient, tokens);
+    }
+}
